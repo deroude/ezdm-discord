3ca7bec6a4dff9531dbd91a1a8ff97b42f7870ed
Valentin Raduti <valentin@thedotin.ro>
2025-02-01 16:39:54 +0200

    different import

--- commands/gen-char.js
+++ commands/gen-char.js
@@ -1,4 +1,4 @@
-import { MessageAttachment } from 'discord.js'
+import discord from 'discord.js'
 export default {
     command: 'gen-char',
     handler: async (genAI, args, message) => {
@@ -8,7 +8,7 @@ export default {
         const race = args.shift();
         const userHint = args.shift();
         const charText = await genAI.generateCharacter(userHint, name, gender, clazz, race);
-        const content = new MessageAttachment(Buffer.from(charText.response.text()), `character-${name || ''}.markdown`)
+        const content = new discord.MessageAttachment(Buffer.from(charText.response.text()), `character-${name || ''}.markdown`)
         message.channel.send({ files: [content] });
     }
 }

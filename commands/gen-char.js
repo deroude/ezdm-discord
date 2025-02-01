666157a171aea45c248fe107a7563399876555b5
Valentin Raduti <valentin@thedotin.ro>
2025-02-01 16:48:02 +0200

    try with attachmentbuilder

--- commands/gen-char.js
+++ commands/gen-char.js
@@ -1,4 +1,4 @@
-import discord from 'discord.js'
+import { AttachmentBuilder } from 'discord.js'
 export default {
     command: 'gen-char',
     handler: async (genAI, args, message) => {
@@ -8,7 +8,7 @@ export default {
         const race = args.shift();
         const userHint = args.shift();
         const charText = await genAI.generateCharacter(userHint, name, gender, clazz, race);
-        const content = new discord.MessageAttachment(Buffer.from(charText.response.text()), `character-${name || ''}.markdown`)
+        const content = AttachmentBuilder(Buffer.from(charText.response.text()), { name: `character-${name || ''}.markdown` })
         message.channel.send({ files: [content] });
     }
 }
